735599728dbb42da25f5b4a8793221c568f636ea
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2025-04-05 16:34:46 +0530

    add suggested users feature; implement user update functionality with password handling and image uploads

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,7 @@
+import bcrypt from "bcryptjs";
+import { v2 as cloudinary } from "cloudinary"; // Importing Cloudinary for image upload
+
+// Models
 import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
@@ -55,6 +59,116 @@ export const followUnfollowUser = async (req, res) => {
     }
   } catch (error) {
     console.error("Error following/unfollowing user:", error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id; // Get the current user's ID from the request
+
+    const usersFollowedByMe = await User.findById(userId).select("following"); // Get the list of users followed by the current user
+
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId }, // Exclude the current user from the suggestions
+        },
+      },
+      { $sample: { size: 10 } }, // Randomly select 10 users
+    ]);
+
+    // Exclude users that are already followed by the current user
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByMe.following.includes(user._id.toString())
+    );
+
+    const suggestedUsers = filteredUsers.slice(0, 4); // Limit to 4 suggested users
+    suggestedUsers.forEach((user) => (user.password = null)); // Remove password field from suggested users
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    console.error("Error fetching suggested users:", error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { fullName, email, username, bio, link, currendPassword, newPassword } =
+    req.body;
+  let { profileImg, coverImg } = req.body;
+
+  const userId = req.user._id; // Get the current user's ID from the request
+
+  try {
+    let user = await User.findById(userId); // Find the user by ID
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    if (
+      (!newPassword && currendPassword) ||
+      (!currendPassword && newPassword)
+    ) {
+      return res
+        .status(400)
+        .json({ error: "Please provide both current and new password" });
+    }
+
+    if (currendPassword && newPassword) {
+      const isPasswordMatch = await bcrypt.compare(
+        currendPassword,
+        user.password
+      ); // Check if the current password matches the stored password
+      if (!isPasswordMatch) {
+        return res.status(400).json({ error: "Current password is incorrect" });
+      }
+      if (newPassword.length < 6) {
+        return res
+          .status(400)
+          .json({ error: "Password must be at least 6 characters" });
+      }
+
+      const salt = await bcrypt.genSalt(10); // Generate a salt for hashing
+      const hashedPassword = await bcrypt.hash(newPassword, salt); // Hash the new password
+      user.password = hashedPassword; // Update the user's password
+    }
+
+    if (profileImg) {
+      // Check if the user already has a profile image
+      if (user.profileImg) {
+        await cloudinary.uploader.destroy(
+          user.profileImg.split("/").pop().split(".")[0]
+        ); // Delete the old profile image from Cloudinary
+      }
+      const uploadedResponse = await cloudinary.uploader.upload(profileImg); // Upload the new profile image to Cloudinary
+      profileImg = uploadedResponse.secure_url; // Get the secure URL of the uploaded image
+    }
+
+    if (coverImg) {
+      // Check if the user already has a cover image
+      if (user.coverImg) {
+        await cloudinary.uploader.destroy(
+          user.coverImg.split("/").pop().split(".")[0]
+        ); // Delete the old cover image from Cloudinary
+      }
+      const uploadedResponse = await cloudinary.uploader.upload(coverImg); // Upload the new cover image to Cloudinary
+      coverImg = uploadedResponse.secure_url; // Get the secure URL of the uploaded image
+    }
+
+    user.fullName = fullName || user.fullName; // Update the user's full name
+    user.email = email || user.email; // Update the user's email
+    user.username = username || user.username; // Update the user's username
+    user.bio = bio || user.bio; // Update the user's bio
+    user.link = link || user.link; // Update the user's link
+    user.profileImg = profileImg || user.profileImg; // Update the user's profile image
+    user.coverImg = coverImg || user.coverImg; // Update the user's cover image
+
+    user = await user.save(); // Save the updated user information
+
+    user.password = null; // Remove the password field from the user object
+
+    return res.status(200).json(user); // Return the updated user information
+  } catch (error) {
+    console.error("Error updating user:", error.message);
     return res.status(500).json({ error: error.message });
   }
 };
